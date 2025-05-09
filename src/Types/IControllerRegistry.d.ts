6c5bc4f34f6b24a78f8033eebf0c3d26ff3d47f8
Boogle <theboogledoo@gmail.com>
2025-05-09 14:44:27 -0500

    eee

--- src/Types/IControllerRegistry.d.ts
+++ src/Types/IControllerRegistry.d.ts
@@ -1,3 +1,3 @@
 import { BaseController } from "../Classes/BaseController";
 
-declare type IControllerRegistry = (new () => BaseController)[];
+declare type IControllerRegistry = ReadonlyArray<new () => BaseController>;
